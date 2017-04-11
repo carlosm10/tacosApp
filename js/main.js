a39acee9e7023a092bc5b1bbf8733cd6d97033bc
Carlos Mondragon <carlosmondragon10@gmail.com>
2017-04-11 13:53:20 -0500

    tmp usuario

--- js/main.js
+++ js/main.js
@@ -1,24 +1,43 @@
+var Usuarios;
 $(function() {
 	var tacosDB = firebase.database().ref('/usuarios');
 
 	tacosDB.on('value', function(data){
-		console.log("value", data.key, data.val());
+		// console.log("value", data.key, data.val());
 		var usuarios = data.val();
 		$.get("views/_tbTacos.html", function(template){
 			$("#rolTable-content").handlebars(template, usuarios);
+			Usuarios = usuarios;
 		})
 		
 	});
 
-	tacosDB.on("child_changed", function(data){
-		console.log("child_changed", data.key, data.val());
+	$(document).on("click", ".avatar", function(){
+		var id = $(this).data("id");
+		var usuario = $.Enumerable.From(Usuarios)
+			.Where(function(el){
+				return el.Value.id === id;
+			}).FirstOrDefault();
+
+		console.log(usuario);
+		$.get("views/Usuario.html", function(template){
+			$("#tacoContainer").handlebars(template, usuario.Value);
+		})
+
+		navigator.vibrate(100);
 	});
+
+	// tacosDB.on("child_changed", function(data){
+	// 	console.log("child_changed", data.key, data.val());
+	// });
 });
 
 
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady(){
+	document.addEventListener("offline", onOffline, false);
+	document.addEventListener("online", onOnline, false);
 	var config = {
         quality: (device.platform !== 'Android') ? 10 : 50,
         destinationType: Camera.DestinationType.DATA_URL,
@@ -36,6 +55,14 @@ function onDeviceReady(){
             }
         });
 	});
+
+	function onOffline(){
+		$("#estatus").text("offline");
+	}	
+
+	function onOnline(){
+		$("#estatus").text("online");
+	}
 
 	function getPicture(d){
 		navigator.camera.getPicture(d.onSuccess,d.onFail, d.config);
